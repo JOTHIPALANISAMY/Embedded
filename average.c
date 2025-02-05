23db2ffd6bc1b031da2763afdeea3d9473ce16f9
JOTHIPALANISAMY <117978130+JOTHIPALANISAMY@users.noreply.github.com>
2025-02-05 15:18:17 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   int num1,num2,num3;
+   float average;
+   num1=98;
+   num2=90;
+   num3=89;
+   average=(float)(num1+num2+num3)/3;
+   printf("average=%f",average);
+}
